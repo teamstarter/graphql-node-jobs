cf6ba54e50fcbeeefc6d9e78d84e24c249b3153f
Vincent Desmares <vincent.desmares@gmail.com>
2020-02-10 10:39:36 +0100

    fix(migration): Make the migration work for postgres and sqlite.

--- migrations/20200210094822-update-job-column-types.js
+++ migrations/20200210094822-update-job-column-types.js
@@ -1,27 +1,53 @@
 module.exports = {
   up: async function(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('job', 'input', {
-      type: Sequelize.JSON,
-      allowNull: true,
-      defaultValue: null
-    })
-    await queryInterface.changeColumn('job', 'output', {
-      type: Sequelize.JSON,
-      allowNull: true,
-      defaultValue: null
-    })
+    if (queryInterface.sequelize.options.dialect === 'postgres') {
+      await queryInterface.changeColumn('job', 'input', {
+        type: 'JSON USING CAST("input" as JSON)',
+        allowNull: true,
+        defaultValue: null
+      })
+      await queryInterface.changeColumn('job', 'output', {
+        type: 'JSON USING CAST("output" as JSON)',
+        allowNull: true,
+        defaultValue: null
+      })
+    } else {
+      await queryInterface.changeColumn('job', 'input', {
+        type: Sequelize.JSON,
+        allowNull: true,
+        defaultValue: null
+      })
+      await queryInterface.changeColumn('job', 'output', {
+        type: Sequelize.JSON,
+        allowNull: true,
+        defaultValue: null
+      })
+    }
   },
 
   down: async function(queryInterface) {
-    await queryInterface.changeColumn('job', 'input', {
-      type: Sequelize.TEXT,
-      allowNull: true,
-      defaultValue: null
-    })
-    await queryInterface.changeColumn('job', 'output', {
-      type: Sequelize.TEXT,
-      allowNull: true,
-      defaultValue: null
-    })
+    if (queryInterface.sequelize.options.dialect === 'postgres') {
+      await queryInterface.changeColumn('job', 'input', {
+        type: 'TEXT USING CAST("input" as TEXT)',
+        allowNull: true,
+        defaultValue: null
+      })
+      await queryInterface.changeColumn('job', 'output', {
+        type: 'TEXT USING CAST("output" as TEXT)',
+        allowNull: true,
+        defaultValue: null
+      })
+    } else {
+      await queryInterface.changeColumn('job', 'input', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        defaultValue: null
+      })
+      await queryInterface.changeColumn('job', 'output', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        defaultValue: null
+      })
+    }
   }
 }
