7898830580276609f05205c21b7ffc2a5da0110b
germainvictor <victor.germain@hetic.net>
2021-05-18 18:45:20 +0200

    Add(toggleHoldJobType): add model & shema & test

---
+++ migrations/20201130112919-create-job-hold-type.js
@@ -0,0 +1,31 @@
+'use strict'
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('jobHoldType', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: function (queryInterface) {
+    return queryInterface.dropTable('jobHoldType')
+  },
+}
