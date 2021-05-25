aee4367adab2e9c76befa80f1d597e2f937a24ac
germainvictor <victor.germain@hetic.net>
2021-05-25 13:49:08 +0200

    add prevent default attribute

--- src/graphql/job.ts
+++ src/graphql/job.ts
@@ -142,7 +142,7 @@ export default function JobConfiguration(
       },
     },
     create: {
-      // preventDuplicateOnAttributes: ['jobUniqueId'],
+      preventDuplicateOnAttributes: ['jobUniqueId'],
     },
   }
 }
