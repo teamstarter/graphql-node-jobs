4063b0433ae23866c4316fde3428dbbf15810149
germainvictor <victor.germain@hetic.net>
2021-06-02 17:53:01 +0200

    add missing await

--- src/graphql/getApolloServer.ts
+++ src/graphql/getApolloServer.ts
@@ -35,7 +35,7 @@ export default async function getApolloServer(
     )
     if (onJobFail) {
       for (const job of jobsFail) {
-        onJobFail(job)
+        await onJobFail(job)
       }
     }
   }
