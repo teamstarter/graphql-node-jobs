a275989eb065709a80dffa1d5afa6fb2f2319fe8
germainvictor <victor.germain@hetic.net>
2021-06-02 11:26:29 +0200

    Add test

---
+++ tests/onServerBooting.spec.js
@@ -0,0 +1,185 @@
+const request = require('supertest')
+const addMinutes = require('date-fns/addMinutes')
+
+const {
+  migrateDatabase,
+  seedDatabase,
+  getNewServer,
+  models,
+  closeEverything,
+  deleteTables,
+  resetDatabase,
+} = require('./test-database.js')
+const {
+  checkForJobs,
+  listJobs,
+  getNewClient,
+  createJob,
+  CancelRequestedError,
+} = require('../lib/index')
+
+// This is the maximum amount of time the band of test can run before timing-out
+jest.setTimeout(600000)
+
+let server = null
+const client = getNewClient(
+  `http://localhost:${process.env.PORT || 8080}/graphql`
+)
+
+const wait = (time) => new Promise((resolve) => setTimeout(resolve, time))
+
+const acquireJob = (variables) => ({
+  query: `mutation($typeList: [String!]!, $workerId: String) {
+    acquireJob(
+      typeList: $typeList
+      workerId: $workerId
+    ) {
+      id
+      name
+      status
+      output
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const jobCreate = (variables) => ({
+  query: `mutation($job: jobInput!) {
+    jobCreate(
+      job: $job
+    ) {
+      id
+      name
+      status
+      output
+      jobUniqueId
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const jobUpdate = (variables) => ({
+  query: `mutation($job: jobInput!) {
+    jobUpdate(
+      job: $job
+    ) {
+      id
+      name
+      status
+      isUpdateAlreadyCalledWhileCancelRequested
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const jobList = (variables) => ({
+  query: `query($where: SequelizeJSON!) {
+    job(
+      where: $where
+    ) {
+      id
+      name
+      status
+      isHighFrequency
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const customAcquire = (variables) => ({
+  query: `mutation($typeList: [String!]!) {
+    customAcquire(
+      typeList: $typeList
+    ) {
+      id
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const recoverJob = (variables) => ({
+  query: `mutation($id: Int!) {
+    recover(id: $id) {
+      id
+      status
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const toggleHoldJobType = (variables) => ({
+  query: `mutation($type: String!) {
+    toggleHoldJobType(type: $type) {
+      id
+      type
+
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+const retryJob = (variables) => ({
+  query: `mutation retryJob($id: Int!){
+    retryJob(id: $id) {
+      id
+      name
+      type
+      status
+    }
+  }`,
+  variables,
+  operationName: null,
+})
+
+/**
+ * Starting the tests
+ */
+describe('Test when server booting', () => {
+  beforeAll(async () => {
+    await migrateDatabase()
+    await seedDatabase()
+    server = await getNewServer()
+  })
+
+  beforeEach(async () => {
+    await resetDatabase()
+  })
+
+  afterEach(async () => {
+    await deleteTables()
+  })
+
+  afterAll(async (done) => {
+    await closeEverything(server, models, done)
+  })
+
+  it('When server booting switch processing job to failed ', async () => {
+    const responseCreateJob = await createJob(client, {
+      type: 'processingToFail',
+      status: 'processing',
+    })
+
+    expect(responseCreateJob.status).toBe('processing')
+
+    await new Promise((resolve) => server.close(() => resolve()))
+    server = await getNewServer()
+
+    const responseJobList = await request(server)
+      .post('/graphql')
+      .send(
+        jobList({
+          where: { type: 'processingToFail' },
+        })
+      )
+
+    expect(responseJobList.body.errors).toBeUndefined()
+    expect(responseJobList.body.data.job[0].status).toBe('failed')
+  })
+})
