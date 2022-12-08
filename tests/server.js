1956436058ddcbe605d8e8c2056ada02adec7c89
Vincent Desmares <vincent.desmares@gmail.com>
2022-12-08 10:52:03 +0100

    Update node version and server test script.

--- tests/server.js
+++ tests/server.js
@@ -2,39 +2,53 @@ const { getApolloServer } = require('./../lib/index')
 const express = require('express')
 const http = require('spdy')
 const { PubSub } = require('graphql-subscriptions')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { WebSocketServer } = require('ws')
+const { json } = require('body-parser')
+const cors = require('cors')
+
 const config = require('./sqliteTestConfig.js')
-const app = express()
 
 async function startServer() {
+  const app = express()
   var options = {
     spdy: {
       plain: true,
     },
   }
+  const httpServer = http.createServer(options, app)
 
   const pubSubInstance = new PubSub()
 
-  const server = await getApolloServer(config, { pubSubInstance })
+  const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: '/graphql',
+  })
+
+  const server = await getApolloServer(config, {
+    pubSubInstance,
+    playground: true,
+  })
 
   /**
    * This is the test server.
    * Used to allow the access to the Graphql Playground at this address: http://localhost:8080/graphql.
    * Each time the server is starter, the database is reset.
    */
-  server.applyMiddleware({
-    app,
-    path: '/graphql',
-  })
+  await server.start()
+
+  app.use('/graphql', cors(), json(), expressMiddleware(server, {}))
 
   const port = process.env.PORT || 8080
 
-  const serverHttp = http.createServer(options, app).listen(port, async () => {
+  httpServer.listen(port, async () => {
     console.log(
       `🚀 http/https/h2 server runs on  http://localhost:${port}/graphql .`
     )
   })
-
-  server.installSubscriptionHandlers(serverHttp)
 }
 
 startServer()
