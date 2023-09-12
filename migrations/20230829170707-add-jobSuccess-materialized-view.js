0b11a7289ebed887126d50dad67c5b4b391b0c07
genouf <gabriel.enouf@yahoo.fr>
2023-09-12 11:45:15 +0200

    Materialized View jobSuccess updated to jobStatsByDay [skip ci]

--- migrations/20230829170707-add-jobSuccess-materialized-view.js
+++ migrations/20230829170707-add-jobSuccess-materialized-view.js
@@ -1,17 +1,18 @@
 module.exports = {
   up: async function (queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
-	CREATE MATERIALIZED VIEW IF NOT EXISTS "jobSuccess" as
+	CREATE MATERIALIZED VIEW IF NOT EXISTS "jobStatsByDay" as
 	SELECT
-    	TO_CHAR(DATE_TRUNC('day', j."createdAt"), 'YYYY/MM/DD') AS "day",
-    	ROUND(
-        	(SUM(CASE WHEN j."status" = 'successful' THEN 1 ELSE 0 END) * 100.0) /
-        	NULLIF(SUM(CASE WHEN j."status" IN ('successful', 'failed') THEN 1 ELSE 0 END), 0)
-    	)::INTEGER AS "successRating"
+		TO_CHAR(DATE_TRUNC('day', j."createdAt"), 'YYYY/MM/DD') AS "day",
+		ROUND(
+			(SUM(CASE WHEN j."status" = 'successful' THEN 1 ELSE 0 END) * 100.0) /
+			NULLIF(SUM(CASE WHEN j."status" IN ('successful', 'failed') THEN 1 ELSE 0 END), 0)
+		)::INTEGER AS "successRating",
+		SUM(CASE WHEN j."status" = 'successful' THEN 1 ELSE 0 END) AS "successfulJobs",
+		SUM(CASE WHEN j."status" = 'failed' THEN 1 ELSE 0 END) AS "failedJobs",
+		COUNT(j."status") AS "totalJobs"
 	FROM
     	job j
-	WHERE
-    	j."status" IN ('successful', 'failed')
 	GROUP BY
     	DATE_TRUNC('day', j."createdAt")
 	ORDER BY
