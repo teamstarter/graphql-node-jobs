8a111863048f204270c916f4d5c6c2dd06a7193f
genouf <gabriel.enouf@yahoo.fr>
2023-10-18 16:21:53 +0200

    fix(wM-Migration): worker monitoring migration fixed to match older db

---
+++ migrations/20230928111522-workerStatus-and-workerType-wM-added.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('workerMonitoring', 'workerStatus', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: true,
+    })
+
+    await queryInterface.addColumn('workerMonitoring', 'workerType', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    })
+
+    await queryInterface.bulkUpdate(
+      'workerMonitoring',
+      { workerStatus: 'deprecated' },
+      { workerStatus: { [Sequelize.Op.is]: null } }
+    )
+
+    await queryInterface.bulkUpdate(
+      'workerMonitoring',
+      { workerType: 'deprecated' },
+      { workerType: { [Sequelize.Op.is]: null } }
+    )
+
+    await queryInterface.changeColumn('workerMonitoring', 'workerStatus', {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    })
+
+    await queryInterface.changeColumn('workerMonitoring', 'workerType', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('workerMonitoring', 'workerStatus')
+    await queryInterface.removeColumn('workerMonitoring', 'workerType')
+  },
+}
