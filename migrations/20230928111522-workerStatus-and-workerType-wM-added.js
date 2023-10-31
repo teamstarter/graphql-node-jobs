15866b5157078b2ffff40a196063d591d4ed963b
JP Boris <jphboris@gmail.com>
2023-10-31 12:00:01 +0100

    fix-migration

--- migrations/20230928111522-workerStatus-and-workerType-wM-added.js
+++ migrations/20230928111522-workerStatus-and-workerType-wM-added.js
@@ -1,26 +1,33 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('workerMonitoring', 'workerStatus', {
-      type: Sequelize.DataTypes.STRING,
-      allowNull: true,
-    })
-
-    await queryInterface.addColumn('workerMonitoring', 'workerType', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    })
-
-    await queryInterface.bulkUpdate(
-      'workerMonitoring',
-      { workerStatus: 'deprecated' },
-      { workerStatus: { [Sequelize.Op.is]: null } }
+    const tableWorkerMonitoring = await queryInterface.describeTable(
+      'workerMonitoring'
     )
+    if (!tableWorkerMonitoring.workerStatus) {
+      await queryInterface.addColumn('workerMonitoring', 'workerStatus', {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      })
 
-    await queryInterface.bulkUpdate(
-      'workerMonitoring',
-      { workerType: 'deprecated' },
-      { workerType: { [Sequelize.Op.is]: null } }
-    )
+      await queryInterface.bulkUpdate(
+        'workerMonitoring',
+        { workerStatus: 'deprecated' },
+        { workerStatus: { [Sequelize.Op.is]: null } }
+      )
+    }
+
+    if (!tableWorkerMonitoring.workerType) {
+      await queryInterface.addColumn('workerMonitoring', 'workerType', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      })
+
+      await queryInterface.bulkUpdate(
+        'workerMonitoring',
+        { workerType: 'deprecated' },
+        { workerType: { [Sequelize.Op.is]: null } }
+      )
+    }
 
     await queryInterface.changeColumn('workerMonitoring', 'workerStatus', {
       type: Sequelize.DataTypes.STRING,
